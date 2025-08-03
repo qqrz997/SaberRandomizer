b754cbe1fad2530baec5b5a835fdf550777373fe
qqrz997 <156676353+qqrz997@users.noreply.github.com>
2025-08-03 23:29:38 +0100

    Use new API

--- SaberRandomizer/App/RandomSaberFactory.cs
+++ SaberRandomizer/App/RandomSaberFactory.cs
@@ -10,22 +10,16 @@ internal class RandomSaberFactory
 {
     private readonly Random random;
     private readonly ISaberFileManager saberFileManager;
-    private readonly ISaberMetadataCache saberMetadataCache;
-    private readonly ISabersLoader sabersLoader;
-    private readonly ITrailFactory trailFactory;
-
+    private readonly ISaberInstanceFactory saberInstanceFactory;
+    
     public RandomSaberFactory(
         Random random,
-        ISaberFileManager saberFileManager,
-        ISaberMetadataCache saberMetadataCache, 
-        ISabersLoader sabersLoader, 
-        ITrailFactory trailFactory)
+        ISaberFileManager saberFileManager, 
+        ISaberInstanceFactory saberInstanceFactory)
     {
         this.random = random;
         this.saberFileManager = saberFileManager;
-        this.saberMetadataCache = saberMetadataCache;
-        this.sabersLoader = sabersLoader;
-        this.trailFactory = trailFactory;
+        this.saberInstanceFactory = saberInstanceFactory;
     }
     
     public async Task<SaberInstanceSet> CreateRandomSaber()
@@ -33,21 +27,11 @@ internal class RandomSaberFactory
         var files = saberFileManager.GetLoadedSaberFiles();
         if (files is [])
         {
-            return CreateDefaultSabers();
+            return await saberInstanceFactory.CreateSaberSet(null, CancellationToken.None);
         }
         
         var randomIndex = random.Next(files.Length);
         var saberFile = files[randomIndex];
-        var saberPrefab = !saberMetadataCache.TryGetMetadata(saberFile.Hash, out var meta) ? null
-            : await sabersLoader.GetSaberData(meta.SaberFile, true, CancellationToken.None);
-
-
-        return saberPrefab?.Prefab?.Instantiate() ?? CreateDefaultSabers();
+        return await saberInstanceFactory.CreateSaberSet(saberFile.Hash, CancellationToken.None);
     }
-
-    private SaberInstanceSet CreateDefaultSabers() =>
-        new(gameResourcesProvider.CreateNewDefaultSaber(),
-            gameResourcesProvider.CreateNewDefaultSaber(),
-            [new DefaultTrailData(gameResourcesProvider.DefaultTrailMaterial)],
-            [new DefaultTrailData(gameResourcesProvider.DefaultTrailMaterial)]);
 }
